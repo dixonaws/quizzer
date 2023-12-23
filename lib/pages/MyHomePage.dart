0eb26947c7f2c6254faed7ae8cc0f9490ae9f6e9
John Dixon <dixonaws@amazon.com>
2023-12-23 12:31:46 -0500

    added reset functionality

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -12,6 +12,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  void reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -42,9 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
             },
           ),
         ),
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -54,33 +58,18 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: const EdgeInsets.all(8.0),
             child: IconButton(
               icon: Icon(
-                Icons.settings,
+                Icons.account_box_rounded,
                 color: Colors.white,
               ),
               onPressed: () {
-                print("Open settings dialog");
+                print("Open accocunt dialog");
               },
             ),
           )
         ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -103,7 +92,17 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(icon: Icon(Icons.reset_tv), label: "Reset"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        onTap: (index) {
+          setState(() {
+            // reset button pressed
+            if (index == 0) {
+              reset();
+            }
+          });
+        },
+        // body:yourWidgetList[activeIndex]
+      ),
     );
   }
 }
